da7b0ade7b5ffd5ebc7672dbeeeb22e3e7ece8a0
Fred Leitz <fred.leitz@gmail.com>
2017-05-05 14:30:47 -0700

    fix yaml loading

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,4 +1,4 @@
 elasticsearch_file = "#{Rails.root}/config/elasticsearch.yml"
 
-config = YAML.safe_load(ERB.new(File.read(elasticsearch_file)).result).symbolize_keys
-Elasticsearch::Model.client = Elasticsearch::Client.new(config)
+config = YAML.load(ERB.new(File.read(elasticsearch_file)).result).symbolize_keys
+Elasticsearch::Model.client = Elasticsearch::Client.new(config[Rails.env])
