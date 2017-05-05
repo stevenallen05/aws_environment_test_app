794bc93d9c6fccf3efce7be15a9b03c9174b39b3
Fred Leitz <fred.leitz@gmail.com>
2017-05-05 14:41:49 -0700

    fix initializer

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,4 +1,4 @@
 elasticsearch_file = "#{Rails.root}/config/elasticsearch.yml"
 
-config = YAML.load(ERB.new(File.read(elasticsearch_file)).result).symbolize_keys
-Elasticsearch::Model.client = Elasticsearch::Client.new(config[Rails.env])
+config = YAML.load(ERB.new(File.read(elasticsearch_file)).result).symbolize_keys[Rails.env]
+Elasticsearch::Model.client = Elasticsearch::Client.new(config) if config
