d72f19f74c72cf75d9042d83e05d6256674ace3b
Fred Leitz <fred.leitz@gmail.com>
2017-05-05 18:59:19 -0700

    add sidekiq web

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
+require 'sidekiq/web'
+mount Sidekiq::Web => '/sidekiq'
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
